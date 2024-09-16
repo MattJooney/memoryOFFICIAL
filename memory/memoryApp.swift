5cff6e80802a8ac0c08564a7acf7a208265442d2
MattJooney <mkang4153@gmail.com>
2024-09-16 14:56:04 -0400

    Initial Commit

---
+++ memory/memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryApp.swift
+//  memory
+//
+//  Created by Matthew Kang on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
