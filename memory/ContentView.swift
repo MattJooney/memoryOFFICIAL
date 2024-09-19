1ebb498fca6c30b1474bfc73cd7174c96ce441ee
MattJooney <mkang4153@gmail.com>
2024-09-19 15:25:21 -0400

    -Organize -Working in both landscape and portrait
    FINAL

--- memory/ContentView.swift
+++ memory/ContentView.swift
@@ -7,6 +7,24 @@
 
 import SwiftUI
 
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
+
 struct ContentView: View {
     let flags: [String] = ["🇰🇷","🇪🇺","🇯🇵","🇺🇸","🇹🇭","🇫🇷","🇨🇮","🇩🇪","🇨🇦","🇦🇺"]
     let sports: [String] = ["⚽️","🏀","🏈","⚾️","🎾","🏐","🏓","🏸","🏒","🏹"]
@@ -19,26 +37,75 @@ struct ContentView: View {
     var body: some View {
         let totalCards: [String] = (theme + theme).shuffled()
         
+        Group {
+            if orientation.isPortrait {
+                verticalGame(deck: totalCards)
+            } else if orientation.isLandscape {
+                horizontalGame(deck: totalCards)
+            } else if orientation.isFlat {
+                verticalGame(deck: totalCards)
+            } else {
+                verticalGame(deck: totalCards)
+            }
+        }
+        .onRotate { newOrientation in
+            orientation = newOrientation
+        }
+        
+    }
+    
+    @ViewBuilder
+    func verticalGame(deck: [String]) -> some View {
+        if(gameStart == false) {
+            title
+            titleScreen(size: 265)
+            buttonRow
+        }
+        else{
+            title
+            deckLayout(cards: deck, pad: 8, gridSize: 90)
+            buttonRow
+        }
+    }
+    
+    @ViewBuilder
+    func horizontalGame(deck: [String]) -> some View {
+        if(gameStart == false) {
+            title
+            titleScreen(size: 70)
+            buttonRow
+        }
+        else{
+            title
+            deckLayout(cards: deck, pad: 0, gridSize: 65)
+            buttonRow
+        }
+    }
+    
+    var title: some View{
         Text("Memory!")
             .imageScale(.large)
             .font(.system(.largeTitle, weight: .bold))
             .padding()
-        
-        if(gameStart == false) {
-            Text("Pick a Theme!")
-                .font(.system(.largeTitle, weight: .bold))
-                .padding(.vertical, 265)
-        }
-        else{
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))], spacing:7) {
-                ForEach(0..<20, id: \.self) {
-                    index in
-                    CardView(image: totalCards[index]).aspectRatio(39/50, contentMode: .fit)
-                }
-                .padding()
+    }
+    
+    func titleScreen(size: CGFloat) -> some View {
+        Text("Pick a Theme!")
+            .font(.system(.largeTitle, weight: .bold))
+            .padding(.vertical, size)
+    }
+    
+    func deckLayout(cards: [String], pad: CGFloat, gridSize: CGFloat) -> some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: gridSize))]) {
+            ForEach(0..<20, id: \.self) {
+                index in
+                CardView(image: cards[index]).aspectRatio(39/50, contentMode: .fit)
             }
+            .padding(pad)
         }
-        
+    }
+    
+    var buttonRow: some View {
         HStack(spacing: 60){
             themeChoose(topic: flags, pic: "flag.circle", label: "Flags")
             themeChoose(topic: sports, pic: "figure.run.circle", label: "Sports")
